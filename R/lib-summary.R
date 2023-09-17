788f7c2962c7c767528c329042f5e89833b6e227
Kaitlin Maciejewski <katemac68@gmail.com>
2023-09-17 12:28:10 -0500

    update docs

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,7 +1,14 @@
-# create function; name same as file
+#' R Library Summary
+#'
+#' Provide a brief summary of package libraries on machine
+#'
+#' @return A `data.frame` containing count of packages per library and location
+#'  of user libraries
+#' @export
+#'
+#' @examples
+#' lib_summary()
 
-# empty function that takes no arguments
-# returns library location and number of packages
 lib_summary <- function() {
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
